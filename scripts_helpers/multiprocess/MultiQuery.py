ddc918d8c2f89c3d2a45f6edd89a9b4a19cae848
Albert Lacambra <albert.lacambra@ion2s.com>
2013-11-18 18:59:27 +0100

    queries and ingestor

---
+++ scripts_helpers/multiprocess/MultiQuery.py
@@ -0,0 +1,16 @@
+import multiprocessing
+
+
+class MultiQuery(multiprocessing.Process):
+
+    def __init__(self, query, db, pipe):
+        self.query = query
+        self.db = db
+        self.pipe = pipe
+        super(MultiQuery, self).__init__()
+
+    def run(self):
+        cur = self.db.cursor()
+        q = cur.execute(self.query)
+        self.pipe.send(cur.fetchall())
+        self.pipe.close()
---
+++ sql-ingestor/src/main/java/poolingpeople/datacrunch/redmine/IsRedmineEntity.java
@@ -0,0 +1,5 @@
+package poolingpeople.datacrunch.redmine;
+
+public interface IsRedmineEntity {
+	public Integer getId();
+}
